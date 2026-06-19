22ca1540b2e5d46b97cdb52fcb1a61ad0ad8ad56
Gaia <gpellegrini3@studenti.uninsubria.it>
2026-06-19 17:47:37 +0200

    - Aggiunta di un metodo (messaggio, causa) nelle eccezioni, già presente nelle altre due classi.

--- src/main/java/server/exception/PreferitiException.java
+++ src/main/java/server/exception/PreferitiException.java
@@ -1,7 +1,12 @@
 package main.java.server.exception;
 
 public class PreferitiException extends RuntimeException {
+
     public PreferitiException(String message) {
         super(message);
+    }
+
+    public PreferitiException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
--- src/main/java/server/exception/RistoranteException.java
+++ src/main/java/server/exception/RistoranteException.java
@@ -1,7 +1,12 @@
 package main.java.server.exception;
 
 public class RistoranteException extends RuntimeException {
+
     public RistoranteException(String message) {
         super(message);
+    }
+
+    public RistoranteException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
