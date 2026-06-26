e87fbd45094ef4656f26323b8de876643780c77f
Gaia <gpellegrini3@studenti.uninsubria.it>
2026-06-26 15:06:47 +0200

    Aggiunti nuovi metodi nel ClientHandler. - Le recensioni hanno tutti i metodi implementati. - Vanno finiti di implementare i metodi di preferiti, ristorante e utente. - Va finiti l'implementazione dei tipi di richiesta per l'utente (ristoratore e cliente)

--- src/main/java/server/network/ClientHandler.java
+++ src/main/java/server/network/ClientHandler.java
@@ -4,6 +4,7 @@ import main.java.server.dao.*;
 import main.java.server.security.PasswordService;
 import main.java.server.service.*;
 import main.java.shared.communication.*;
+import main.java.shared.domain.Preferito;
 import main.java.shared.domain.Recensione;
 import main.java.shared.domain.Ristorante;
 import main.java.shared.dto.*;
@@ -32,6 +33,7 @@ public class ClientHandler extends Thread{
         this.preferitiService = new PreferitiService(new PreferitiDAO());
     }
 
+    //Metodo per gestire le richieste in arrivo dal client
     @Override
     public void run() {
         try {
@@ -58,6 +60,7 @@ public class ClientHandler extends Thread{
         }
     }
 
+    //Metodo per chiudere la connessione con il client
     private void chiudiConnessione() {
         try {
             if (in != null) {
@@ -76,12 +79,13 @@ public class ClientHandler extends Thread{
         }
     }
 
+    //Metodo per gestire le richieste in arrivo dal client
     public Risposta gestisciRichiesta(Richiesta richiesta) {
         if (richiesta == null || richiesta.getTipoRichiesta() == null) {
             return new Risposta(false, null, "Richiesta non valida");
         }
 
-
+        //Tipi di richiesta
         switch (richiesta.getTipoRichiesta()) {
             case LOGIN:
                 return gestisciLogin(richiesta.getContenuto());
@@ -91,12 +95,36 @@ public class ClientHandler extends Thread{
 
             case LOGOUT:
                 return new Risposta(true, null, "Logout effettuato");
+            //Non sono sicura sia giusto
 
+            //RISTORANTE
             case GET_RISTORANTE:
                 return gestisciGetRistorante(richiesta.getContenuto());
 
+            case CREA_RISTORANTE:
+                return gestisciCreaRistorante(richiesta.getContenuto());
+
+            case AGGIORNA_RISTORANTE:
+                return gestisciModificaRistorante(richiesta.getContenuto());
+
+            case ELIMINA_RISTORANTE:
+                return gestisciEliminaRistorante(richiesta.getContenuto());
+
+            case CERCA_RISTORANTE:
+                return gestisciCercaRistorante(richiesta.getContenuto());
+
+            case ESISTE_RISTORANTE:
+                return gestisciEsistenzaRistorante(richiesta.getContenuto());
+
+            //RECENSIONI
             case GET_RECENSIONI_RISTORANTE:
                 return gestisciGetRecensioniRistorante(richiesta.getContenuto());
+
+            case GET_RECENSIONI_UTENTE:
+                return gestisciGetRecensioniUtente(richiesta.getContenuto());
+
+            case GET_RECENSIONI_BYEMAIL:
+                return gestisciGetRecensioniEmail(richiesta.getContenuto());
 
             case SCRIVI_RECENSIONE:
                 return gestisciScriviRecensione(richiesta.getContenuto());
@@ -107,18 +135,36 @@ public class ClientHandler extends Thread{
             case ELIMINA_RECENSIONE:
                 return gestisciEliminaRecensione(richiesta.getContenuto());
 
+            case RISPONDI_RECENSIONE:
+                return gestisciRispondiRecensione(richiesta.getContenuto());
+
+            case MODIFICA_RISPOSTA:
+                return gestisciModificaRisposta(richiesta.getContenuto());
+
+            case RIEPILOGO_RECENSIONE:
+                return gestisciRiepilogoRecensioni(richiesta.getContenuto());
+
+            //PREFERITI
+            case GET_PREFERITI:
+                return gestisciGetPreferiti(richiesta.getContenuto());
+
             case AGGIUNGI_PREFERITO:
                 return gestisciAggiungiPreferito(richiesta.getContenuto());
 
             case RIMUOVI_PREFERITO:
                 return gestisciRimuoviPreferito(richiesta.getContenuto());
 
-            case RISPONDI_RECENSIONE:
-                return gestisciRispondiRecensione(richiesta.getContenuto());
+            case ESISTE_PREFERITO:
+                return gestisciEsistenzaPreferiti(richiesta.getContenuto());
 
+            case TOGGLE_PREFERITO:
+                return gestisciTogglePreferito(richiesta.getContenuto());
+
+            //TIPO CUCINA
             case GET_TIPO_CUCINA:
                 return gestisciGetTipoCucina();
 
+            //CHIUSURA SERVER
             case SHUTDOWN_SERVER:
                 return gestisciShutdown();
             default:
@@ -127,6 +173,7 @@ public class ClientHandler extends Thread{
     }
 
     //Metodi per gestire le richieste
+    //UTENTE
     private Risposta gestisciLogin(Object contenuto) {
         if (!(contenuto instanceof LoginDTO loginDTO)) {
             return new Risposta(false, null, "Dati login non validi");
@@ -153,6 +200,7 @@ public class ClientHandler extends Thread{
         return new Risposta(false, null, "Registrazione fallita");
     }
 
+    //RISTORANTE
     private Risposta gestisciGetRistorante(Object contenuto) {
         if(!(contenuto instanceof RistoranteDTO dto)){
             return new Risposta(false, null, "Dati ricerca non validi");
@@ -163,7 +211,41 @@ public class ClientHandler extends Thread{
         }
     }
 
+    public Risposta gestisciCreaRistorante(Object contenuto) {
+        return new Risposta(true, null, "Metodo non ancora implementato");
+    }
+
+    public Risposta gestisciModificaRistorante(Object contenuto) {
+        return new Risposta(true, null, "Metodo non ancora implementato");
+    }
+
+    public Risposta gestisciEliminaRistorante(Object contenuto) {
+        return new Risposta(true, null, "Metodo non ancora implementato");
+    }
+
+    public Risposta gestisciCercaRistorante(Object contenuto) {
+        return new Risposta(true, null, "Metodo non ancora implementato");
+    }
+
+    public Risposta gestisciEsistenzaRistorante(Object contenuto) {
+        return new Risposta(true, null, "Metodo non ancora implementato");
+    }
+
+    //RECENSIONI
+
+    //Recensioni del ristorante per il ristoratore
     private Risposta gestisciGetRecensioniRistorante(Object contenuto) {
+        if(!(contenuto instanceof Integer idRistorante)){
+            return new Risposta(false, null, "Dati ricerca non validi");
+        }
+        else{
+            List<Recensione> recensioni = recensioneService.getRecensioni(idRistorante);
+            return new Risposta(true, recensioni, "Recensioni trovate");
+        }
+    }
+
+    //Recensioni del ristorante per il cliente
+    public Risposta gestisciGetRecensioniUtente(Object contenuto) {
         if(!(contenuto instanceof Integer idRistorante)){
             return new Risposta(false, null, "Dati ricerca non validi");
         }
@@ -171,7 +253,17 @@ public class ClientHandler extends Thread{
             List<Recensione> recensioni = recensioneService.getRecensioniRistorante(idRistorante);
             return new Risposta(true, recensioni, "Recensioni trovate");
         }
+    }
 
+    //Recensioni del ristorante dall'email per il cliente
+    public Risposta gestisciGetRecensioniEmail(Object contenuto) {
+        if(!(contenuto instanceof String email)){
+            return new Risposta(false, null, "Dati ricerca non validi");
+        }
+        else{
+            List<Recensione> recensioni = recensioneService.getRecensioniUtente(email);
+            return new Risposta(true, recensioni, "Recensioni trovate");
+        }
     }
 
     private Risposta gestisciScriviRecensione(Object contenuto) {
@@ -205,38 +297,38 @@ public class ClientHandler extends Thread{
         }
     }
 
-    private Risposta gestisciModificaRecensione(Object contenuto){
-        if(!(contenuto instanceof RecensioneDTO dto)){
-            return new Risposta(false, null, "Dati ricerca non validi");
+    private Risposta gestisciModificaRecensione(Object contenuto) {
+        if (!(contenuto instanceof RecensioneDTO dto)) {
+            return new Risposta(false, null, "Dati recensione non validi");
         }
-        else{
-            try{
-                RecensioneDTO recDto = (RecensioneDTO) contenuto;
 
-                boolean testoAggiornato = recensioneService.modificaRecensione(
-                        recDto.getIdRistorante(),
-                        recDto.getEmail(),
-                        recDto.getRecensione()
-                );
-                boolean valutazioneAggiornata = recensioneService.modificaValutazione(
-                        recDto.getIdRistorante(),
-                        recDto.getEmail(),
-                        recDto.getValutazione()
-                );
+        try {
+            boolean testoAggiornato = recensioneService.modificaRecensione(
+                    dto.getIdRistorante(),
+                    dto.getEmail(),
+                    dto.getRecensione()
+            );
 
-                boolean successo = testoAggiornato && valutazioneAggiornata;
+            boolean valutazioneAggiornata = recensioneService.modificaValutazione(
+                    dto.getIdRistorante(),
+                    dto.getEmail(),
+                    dto.getValutazione()
+            );
 
-                return new Risposta(
-                        successo,
-                        null,
-                        successo
-                                ? "Recensione modificata correttamente"
-                                : "Errore durante la modifica della recensione"
-                );
+            boolean successo = testoAggiornato && valutazioneAggiornata;
 
-            } catch (Exception e) {
-                return new Risposta(false, null, e.getMessage());
-            }
+            return new Risposta(
+                    successo,
+                    null,
+                    successo
+                            ? "Recensione modificata correttamente"
+                            : "Errore durante la modifica della recensione"
+            );
+
+        } catch (IllegalArgumentException e) {
+            return new Risposta(false, null, e.getMessage());
+        } catch (Exception e) {
+            return new Risposta(false, null, "Errore interno: " + e.getMessage());
         }
     }
 
@@ -264,6 +356,79 @@ public class ClientHandler extends Thread{
             } catch (Exception e) {
                 return new Risposta(false, null, e.getMessage());
             }
+        }
+    }
+
+    private Risposta gestisciRispondiRecensione(Object contenuto) {
+        if(!(contenuto instanceof RecensioneDTO dto)){
+            return new Risposta(false, null, "Dati ricerca non validi");
+        }
+        try {
+            boolean successo = recensioneService.rispostaRecensione(
+                    dto.getIdRistorante(),
+                    dto.getEmail(),
+                    dto.getRisposta()
+            );
+
+            return new Risposta(
+                    successo,
+                    null,
+                    successo ? "Risposta alla recensione inserita con successo"
+                            : "Impossibile rispondere alla recensione"
+            );
+
+        } catch (IllegalArgumentException e) {
+            return new Risposta(false, null, e.getMessage());
+        }
+    }
+
+    public Risposta gestisciModificaRisposta(Object contenuto) {
+        if(!(contenuto instanceof RecensioneDTO dto)){
+            return new Risposta(false, null, "Dati ricerca non validi");
+        }
+        try {
+            boolean successo = recensioneService.aggiornaRisposta(
+                    dto.getIdRistorante(),
+                    dto.getEmail(),
+                    dto.getRisposta()
+            );
+            return new Risposta(successo, null,
+                    successo ? "Risposta aggiornata con successo"
+                            : "Impossibile aggiornare la risposta"
+            );
+
+        } catch (Exception e) {
+            return new Risposta(false, null, e.getMessage());
+        }
+    }
+
+    public Risposta gestisciRiepilogoRecensioni(Object contenuto) {
+        if (!(contenuto instanceof Integer idRistorante)) {
+            return new Risposta(false, null, "Id ristorante non valido");
+        }
+
+        try {
+            RiepilogoRecensioniDTO riepilogo = recensioneService.getRiepilogo(idRistorante);
+
+            if (riepilogo == null) {
+                return new Risposta(false, null, "Nessun riepilogo disponibile");
+            }
+
+            return new Risposta(true, riepilogo, "Riepilogo recensioni recuperato");
+
+        } catch (Exception e) {
+            return new Risposta(false, null, "Errore nel recupero del riepilogo: " + e.getMessage());
+        }
+    }
+
+    //PREFERITI
+    private Risposta gestisciGetPreferiti(Object contenuto){
+        if(!(contenuto instanceof String email)){
+            return new Risposta(false, null, "Dati ricerca non validi");
+        }
+        else{
+            List<Preferito> preferiti = preferitiService.getPreferitiUtente(email);
+            return new Risposta(true, preferiti, "Preferiti trovate");
         }
     }
 
@@ -301,10 +466,15 @@ public class ClientHandler extends Thread{
         }
     }
 
-    private Risposta gestisciRispondiRecensione(Object contenuto) {
-       return new Risposta(false, null, "Rispondi recensione non supportato");
+    private Risposta gestisciEsistenzaPreferiti(Object contenuto){
+        return new Risposta(true, null, "Metodo non ancora implementato");
     }
 
+    private Risposta gestisciTogglePreferito(Object contenuto){
+        return new Risposta(true, null, "Metodo non ancora implementato");
+    }
+
+    //TIPO CUCINA
     public Risposta gestisciGetTipoCucina(){
         if(!(tipoCucinaService instanceof TipoCucinaService tipoCucinaService)){
             return new Risposta(false, null, "Errore nel recupero dei tipi di cucina");
@@ -314,6 +484,7 @@ public class ClientHandler extends Thread{
             return new Risposta(true, tipi, "Tipi di cucina recuperati");
         }
     }
+
 
     private Risposta gestisciShutdown() {
 
