b47bd2cd4e391870683ff5e2172c2e5fe0866cbc
Cattank04 <giacomo.tancredi06@gmail.com>
2026-07-09 11:56:33 +0200

    fatta la AggiungiRistoranteGUI aggiornato da fare progetto.txt

--- src/main/java/client/gui/azioniRistoratore/AggiungiRistoranteGUI.java
+++ src/main/java/client/gui/azioniRistoratore/AggiungiRistoranteGUI.java
@@ -3,11 +3,18 @@ package main.java.client.gui.azioniRistoratore;
 import main.java.client.gui.TemplateGUI;
 import main.java.client.gui.azioniLoggato.VisualizzaProfiloGUI;
 import main.java.client.gui.menu.RistoratoreGUI;
+import main.java.client.network.ClientConnection;
 import main.java.server.service.UtenteService;
+import main.java.shared.communication.Richiesta;
+import main.java.shared.communication.Risposta;
+import main.java.shared.communication.TipoRichieste;
 import main.java.shared.dto.RistoranteDTO;
+import main.java.shared.dto.TipoCucinaDTO;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.util.List;
 
 public class AggiungiRistoranteGUI extends TemplateGUI {
     private final JTextField campoNomeRistorante;
@@ -16,61 +23,16 @@ public class AggiungiRistoranteGUI extends TemplateGUI {
     private final JTextField campoVia;
     private final JTextField campoNumeroCivico;
     private final JTextField campoFasciaPrezzo;
-    private final JComboBox campoTipoCucina;
-    private final JRadioButton campoDelivery;
-    private final JRadioButton campoPrenotazioneOnline;
-    private final JButton aggiungiRistorante;
+    private final JComboBox<Object> campoTipoCucina;
+    private final JRadioButton radioDeliverySi;
+    private final JRadioButton radioDeliveryNo;
+    private final JRadioButton radioPrenotazioneSi;
+    private final JRadioButton radioPrenotazioneNo;
+    private final JButton btnCrea;
 
     public AggiungiRistoranteGUI(JFrame frame, UtenteService utenteService, String email) {
         super(frame);
         this.frame = frame;
-        //creazione pannello centrale
-        JPanel pannelloCentrale = new JPanel();
-        pannelloCentrale.setLayout(new BoxLayout(pannelloCentrale, BoxLayout.Y_AXIS));
-        pannelloCentrale.setBorder(BorderFactory.createEmptyBorder(40, 60, 40, 60));
-        Dimension dimensioneCampo = new Dimension(300, 40);
-
-        campoNomeRistorante = new JTextField();
-
-        campoNomeRistorante.setEditable(true);
-        campoCitta = new JTextField();
-        campoCitta.setEditable(true);
-        campoNazione = new JTextField();
-        campoNazione.setEditable(true);
-        campoVia = new JTextField();
-        campoVia.setEditable(true);
-        campoNumeroCivico = new JTextField();
-        campoNumeroCivico.setEditable(true);
-        campoFasciaPrezzo = new JTextField();
-        campoFasciaPrezzo.setEditable(true);
-        campoDelivery = new JRadioButton("Servizio delivery");
-        campoDelivery.setSelected(true);
-        campoPrenotazioneOnline= new JRadioButton("Servizio prenotazione online");
-        campoPrenotazioneOnline.setSelected(true);
-        campoTipoCucina = new JComboBox();
-        campoTipoCucina.setEditable(true);
-        aggiungiRistorante = new JButton("Aggiungi Ristorante");
-        aggiungiRistorante.addActionListener(e ->{
-            //RistoranteDTO ristorante = new RistoranteDTO(campoNomeRistorante.getText(), email, campoCitta.getText(), campoNumeroCivico.getText(), campoFasciaPrezzo.getText(), );
-        });
-        //AGGIUNGERE QUERY PER PRENDERE I TIPI DI CUCINA
-
-
-        pannelloCentrale.add(campoNomeRistorante);
-        pannelloCentrale.add(campoCitta);
-        pannelloCentrale.add(campoNazione);
-        pannelloCentrale.add(campoVia);
-        pannelloCentrale.add(campoNumeroCivico);
-        pannelloCentrale.add(campoFasciaPrezzo);
-        pannelloCentrale.add(campoDelivery);
-        pannelloCentrale.add(campoPrenotazioneOnline);
-        pannelloCentrale.add(campoTipoCucina);
-        pannelloCentrale.add(aggiungiRistorante);
-
-
-
-        this.add(pannelloCentrale, BorderLayout.CENTER);
-        JButton button = new JButton("Aggiungi Ristorante");
 
         visualizzaProfilo.addActionListener(e -> {
             frame.setContentPane(new VisualizzaProfiloGUI(frame, utenteService, email));
@@ -86,5 +48,203 @@ public class AggiungiRistoranteGUI extends TemplateGUI {
             frame.repaint();
         });
         pannello.add(home);
+
+        JPanel centro = new JPanel(new GridBagLayout());
+        centro.setBorder(BorderFactory.createEmptyBorder(40, 60, 40, 60));
+        add(centro, BorderLayout.CENTER);
+
+        Border bordoTitolo = BorderFactory.createTitledBorder("Registra il tuo ristorante");
+        Border margineInterno = BorderFactory.createEmptyBorder(20, 15, 20, 15); //valori per la distanza tra il bordo e i componenti centrali
+        centro.setBorder(BorderFactory.createCompoundBorder(bordoTitolo, margineInterno));
+
+        GridBagConstraints vincoloGriglia = new GridBagConstraints();
+        vincoloGriglia.insets = new Insets(10, 10, 10, 10);
+        vincoloGriglia.anchor = GridBagConstraints.WEST;
+        vincoloGriglia.fill = GridBagConstraints.HORIZONTAL;
+
+        //popolamento righe e colonne
+        //riga 0 - nome ristorante
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 0;
+        vincoloGriglia.weightx = 0; //la colonna non si allarga
+        centro.add(new JLabel("Inserisci il nome del ristorante: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        this.campoNomeRistorante = new JTextField();
+        centro.add(campoNomeRistorante, vincoloGriglia);
+
+        //riga 1 - città
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 1;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Inserisci la città: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        this.campoCitta = new JTextField(20);
+        centro.add(campoCitta, vincoloGriglia);
+
+        //riga 2 - Nazione
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 2;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Inserisci la nazione: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        this.campoNazione = new JTextField(20);
+        centro.add(campoNazione, vincoloGriglia);
+
+        //Riga 3 - via
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 3;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Inserisci la via: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        this.campoVia = new JTextField(20);
+        centro.add(campoVia, vincoloGriglia);
+
+        //Riga 4 - numero civico
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 4;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Inserisci il numero civico: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        this.campoNumeroCivico = new JTextField(3);
+        centro.add(campoNumeroCivico, vincoloGriglia);
+
+        //riga 5 - FasciaPrezzo
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 5;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Prezzo massimo: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        this.campoFasciaPrezzo = new JTextField(3);
+        centro.add(campoFasciaPrezzo, vincoloGriglia);
+
+        //riga 6 - servizio delivery
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 6;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Il tuo ristorante effettua il servizio delivery?"), vincoloGriglia);
+        //creazione pannello per i JRadio del delivery
+        JPanel pannelloDelivery = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        this.radioDeliverySi = new JRadioButton("Si");
+        this.radioDeliveryNo = new JRadioButton("No");
+        radioDeliverySi.setSelected(true);
+        //raggruppo i bottoni della sezione delivery
+        ButtonGroup gruppoBottoniDelivery = new ButtonGroup();
+        gruppoBottoniDelivery.add(radioDeliverySi);
+        gruppoBottoniDelivery.add(radioDeliveryNo);
+
+        pannelloDelivery.add(radioDeliverySi);
+        pannelloDelivery.add(radioDeliveryNo);
+
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        centro.add(pannelloDelivery, vincoloGriglia);
+
+        //riga 7 - servizio prenotazione online
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 7;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Il tuo ristorante permette le prenotazioni online?"), vincoloGriglia);
+        //pannello per il gruppo bottoni per prenotazione online
+        JPanel pannelloPrenotazione = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        this.radioPrenotazioneSi = new JRadioButton("Si");
+        this.radioPrenotazioneNo = new JRadioButton("No");
+        this.radioPrenotazioneSi.setSelected(true);
+
+        //creazione ButtonGroup
+        ButtonGroup gruppoBottoniPrenotazione = new ButtonGroup();
+        gruppoBottoniPrenotazione.add(radioPrenotazioneSi);
+        gruppoBottoniPrenotazione.add(radioPrenotazioneNo);
+
+        pannelloPrenotazione.add(radioPrenotazioneSi);
+        pannelloPrenotazione.add(radioPrenotazioneNo);
+
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        centro.add(pannelloPrenotazione, vincoloGriglia);
+
+        //riga 8 - tipo cucina
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 8;
+        vincoloGriglia.weightx = 0;
+        centro.add(new JLabel("Che tipo di cucina offre il tuo ristorante?: "), vincoloGriglia);
+        vincoloGriglia.gridx = 1;
+        vincoloGriglia.weightx = 0;
+        campoTipoCucina = new JComboBox<>();
+        caricaTipiCucina();
+        centro.add(campoTipoCucina, vincoloGriglia);
+
+        //riga 9 - bottone creazione
+        vincoloGriglia.gridx = 0;
+        vincoloGriglia.gridy = 9;
+        vincoloGriglia.gridwidth = 2;
+        vincoloGriglia.fill = GridBagConstraints.NONE;  //permette di non riempire orizzontalmente
+        vincoloGriglia.anchor = GridBagConstraints.CENTER;
+        this.btnCrea = new JButton("Crea ristorante");
+        centro.add(btnCrea, vincoloGriglia);
+
+        btnCrea.addActionListener(e -> eseguiCreazioneRistorante(email, utenteService));
+    }
+
+    private void eseguiCreazioneRistorante(String email, UtenteService utenteService) {
+        String nomeRistorante = campoNomeRistorante.getText().trim();
+        String citta = campoCitta.getText().trim();
+        String nazione = campoNazione.getText().trim();
+        String via = campoVia.getText().trim();
+        int numeroCivico = Integer.parseInt(campoNumeroCivico.getText().trim());
+        int fasciaPrezzo = Integer.parseInt(campoFasciaPrezzo.getText().trim());
+        boolean delivery = this.radioDeliverySi.isSelected()? true : false;
+        boolean prenotazioneOnline = this.radioPrenotazioneSi.isSelected()? true : false;
+        int indiceSelezionato = campoTipoCucina.getSelectedIndex();
+        Integer tipoCucina;
+            /*if (indiceSelezionato <= 0) {
+                tipoCucina = null; // "Qualsiasi" -> nessun filtro
+            } else {
+                TipoCucinaDTO tipoSelezionato = (TipoCucinaDTO) campoTipoCucina.getSelectedItem();
+                tipoCucina = tipoSelezionato.getId(); // adatta al metodo reale del DTO
+            }*/
+
+        //DA CONTROLLARE
+        TipoCucinaDTO tipoSelezionato = (TipoCucinaDTO) campoTipoCucina.getSelectedItem();
+        tipoCucina = tipoSelezionato.getId(); // adatta al metodo reale del DTO
+
+        RistoranteDTO dtoRisotrante = new RistoranteDTO(nomeRistorante, email, citta, nazione,
+                via, numeroCivico, fasciaPrezzo, delivery, prenotazioneOnline, tipoCucina);
+        Richiesta richiesta = new Richiesta(TipoRichieste.CREA_RISTORANTE, dtoRisotrante);
+        Risposta risposta = ClientConnection.inviaRichiesta(richiesta);
+
+        if(risposta != null && risposta.getSuccesso()) {
+            frame.setContentPane(new RistoratoreGUI(frame, utenteService, email));
+            frame.revalidate();
+            frame.repaint();
+        } else {
+            String messaggioErrore = (risposta != null) ? risposta.getMsg() : "Impossibile contattare il server";
+            JOptionPane.showMessageDialog(this, messaggioErrore, "Errore", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void caricaTipiCucina() {
+        Richiesta richiesta = new Richiesta(TipoRichieste.GET_TIPO_CUCINA, null);
+        Risposta risposta = ClientConnection.inviaRichiesta(richiesta);
+        if (risposta != null && risposta.getSuccesso()) {
+            @SuppressWarnings("unchecked")
+            java.util.List<TipoCucinaDTO> tipiCucina = (List<TipoCucinaDTO>) risposta.getContenuto();
+            for (TipoCucinaDTO tipo : tipiCucina) {
+                campoTipoCucina.addItem(tipo);
+            }
+        } else {
+            String messaggioErrore;
+            if (risposta != null) {
+                messaggioErrore = risposta.getMsg();
+            } else {
+                messaggioErrore = "Impossibile contattare il server";
+            }
+            JOptionPane.showMessageDialog(this, messaggioErrore, "Errore", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
